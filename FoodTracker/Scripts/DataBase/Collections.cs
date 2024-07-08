adee719bbde2713be73b6351dab5f9c9d9724b30
Anthony <anthonymellon@yahoo.com>
2024-07-09 01:36:29 +1200

    Adda a page to create food items and send them up the the db

--- FoodTracker/Scripts/DataBase/Collections.cs
+++ FoodTracker/Scripts/DataBase/Collections.cs
@@ -5,7 +5,7 @@ namespace FoodTracker.Scripts.DataBase
     public class MongoFoodItem
     {
         public ObjectId Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public double Calories { get; set; }
         public double Protein { get; set; }
         public double Carbs { get; set; }
