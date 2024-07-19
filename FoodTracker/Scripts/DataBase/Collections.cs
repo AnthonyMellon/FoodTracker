d097f56b8e49e3fa64a0664dbdc535585646f15b
Anthony <anthonymellon@yahoo.com>
2024-07-20 01:10:44 +1200

    setup classes for Meals and Days

--- FoodTracker/Scripts/DataBase/Collections.cs
+++ FoodTracker/Scripts/DataBase/Collections.cs
@@ -11,4 +11,19 @@ namespace FoodTracker.Scripts.DataBase
         public double Carbs { get; set; }
         public double Fat { get; set; }
     }
+
+    public class MongoMeal
+    {
+        public ObjectId Id { get; set; }
+        public string? Name { get; set; }
+        public ObjectId[]? foodItems { get; set; }        
+    }
+
+    public class MongoDay
+    {
+        public ObjectId Id { get; set; }
+        public DateTime? date { get; set; }
+        public ObjectId[]? foodItems { get; set; }
+        public ObjectId[]? meals { get; set; }
+    }
 }
